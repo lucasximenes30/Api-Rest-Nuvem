8d8acc92ac2c6f32ab7296b9e82aa4a7d5602f3c
Lucas Ximenes <lucasximenes177@gmail.com>
2025-07-23 16:13:02 -0300

    feat: criando o repository

---
+++ src/main/java/com/apirest/ApiRestNuvem/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.apirest.ApiRestNuvem.domain.repository;
+
+
+import com.apirest.ApiRestNuvem.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
