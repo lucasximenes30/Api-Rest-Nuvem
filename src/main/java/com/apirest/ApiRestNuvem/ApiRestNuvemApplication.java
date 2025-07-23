98a870108889a33ce68e8e6850236c4483fbd395
lucasximenes30 <lucasximenes177@gmail.com>
2025-07-23 10:25:04 -0300

    first commit

---
+++ src/main/java/com/apirest/ApiRestNuvem/ApiRestNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.ApiRestNuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirest/ApiRestNuvem/ApiRestNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.ApiRestNuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
